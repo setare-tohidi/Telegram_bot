be561883205c87df599970c5b9c9225f1626a717
httpseee <setare.tohidi@gmail.com>
2024-03-18 21:59:09 -0400

    eval calculator

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,23 @@
 import telebot
 import os
+from loguru import logger
 
 # export inconnu_bot_token=7064417063:AAF82dz9rUlVfd8tA_19mu4qUJ2gJ1wwAIs
 bot = telebot.TeleBot(os.environ['inconnu_bot_token']) # You can set parse_mode by default. HTML or MARKDOWN
+
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+ 
+ 
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+ 
+ 
+logger.info('Bot started')
+bot.infinity_polling()
+logger.info("Done!")
+ 
